90349b4cbff51e63c56b8904fe9acca40e5bc03e
Emil <maxmudov2000@gmail.com>
2024-09-04 20:59:27 +0400

    city search toggler added

---
+++ src/components/city-and-date/CitySearch.jsx
@@ -0,0 +1,15 @@
+function CitySearch() {
+    return (
+        <section className="city-search">
+            <h5>Search</h5>
+            <input type="text" placeholder="Type here..."/>
+            <ul>
+                <li><button>Baku</button></li>
+                <li><button>Baku</button></li>
+                <li><button>Baku</button></li>
+            </ul>
+        </section>
+    )
+}
+
+export default CitySearch
